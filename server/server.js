cd7cf397b21b3a0acda26a02eff0bb50fcd25d2a
tabishcode <tabishakhtar007860@gmail.com>
2025-04-26 01:55:25 +0500

    feat(auth): complete backend authentication (register, login, logout, middleware)

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+require("dotenv").config();
+const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const authRouter = require("./routes/auth/auth-routes");
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB connected"))
+.catch((error)=> console.log("MonogoDB connection problem", error))
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma"],
+    credentials: true
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));

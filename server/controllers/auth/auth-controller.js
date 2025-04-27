31e511d2158a6e18b678220003057882c81ea949
tabishcode <tabishakhtar007860@gmail.com>
2025-04-27 15:24:30 +0500

    fronted code

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -4,35 +4,45 @@ const User = require("../../models/User");
 
 //register
 const registerUser = async (req, res) => {
-  const { userName, email, password } = req.body;
+  // const { userName, email, password } = req.body;
 
-  try {
-    const checkUser = await User.findOne({ email });
-    if (checkUser)
-      return res.json({
-        success: false,
-        message: "User Already exists with the same email! Please try again",
-      });
+  // try {
+  //   const checkUser = await User.findOne({ email });
+  //   if (checkUser)
+  //     return res.json({
+  //       success: false,
+  //       message: "User Already exists with the same email! Please try again",
+  //     });
 
-    const hashPassword = await bcrypt.hash(password, 12);
+    const hashPassword = await bcrypt.hash('admin', 12);
+  //   const newUser = new User({
+  //     userName,
+  //     email,
+  //     password: hashPassword,
+  //   });
+
+
     const newUser = new User({
-      userName,
-      email,
+      userName: "admin",
+      email: "admin@example.com",
       password: hashPassword,
+      role: "admin", // <-- Key point
     });
+  
+
 
     await newUser.save();
     res.status(200).json({
       success: true,
       message: "Registration successful",
     });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured",
-    });
-  }
+  // } catch (e) {
+  //   console.log(e);
+  //   res.status(500).json({
+  //     success: false,
+  //     message: "Some error occured",
+  //   });
+  // }
 };
 
 //login
